db4b7718ec1c05c7f6cd7510311d6d731e0c85d1
Hacheron <109388172+Hacheron@users.noreply.github.com>
2023-03-18 08:47:18 -0600

    Add files via upload

---
+++ CalculatorGUI.java
@@ -0,0 +1,186 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.w3c.dom.*;
+
+public class CalculatorGUI extends JFrame implements ActionListener {
+    private JTextField num1Field, num2Field, resultField;
+    private JButton addButton, subtractButton, multiplyButton, divideButton;
+    private JButton loadButton, saveButton;
+    private File inputFile, outputFile;
+
+    public CalculatorGUI() {
+        // Set up the frame
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        // Create the input fields
+        num1Field = new JTextField(10);
+        num2Field = new JTextField(10);
+        resultField = new JTextField(10);
+        resultField.setEditable(false);
+
+        // Create the buttons
+        addButton = new JButton("+");
+        subtractButton = new JButton("-");
+        multiplyButton = new JButton("*");
+        divideButton = new JButton("/");
+        loadButton = new JButton("Load XML");
+        saveButton = new JButton("Process XML");
+
+        // Add action listeners to the buttons
+        addButton.addActionListener(this);
+        subtractButton.addActionListener(this);
+        multiplyButton.addActionListener(this);
+        divideButton.addActionListener(this);
+        loadButton.addActionListener(this);
+        saveButton.addActionListener(this);
+
+        // Add the components to the content pane
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        contentPane.setLayout(new GridLayout(10, 2, 5, 5));
+        contentPane.add(new JLabel("First number:"));
+        contentPane.add(num1Field);
+        contentPane.add(new JLabel("Second number:"));
+        contentPane.add(num2Field);
+        contentPane.add(new JLabel("Operation:"));
+        contentPane.add(addButton);
+        contentPane.add(new JLabel(""));
+        //contentPane.add(subtractButton);
+        //contentPane.add(new JLabel(""));
+        //contentPane.add(multiplyButton);
+        //contentPane.add(new JLabel(""));
+        //contentPane.add(divideButton);
+        contentPane.add(new JLabel("Result:"));
+        contentPane.add(resultField);
+        contentPane.add(loadButton);
+        contentPane.add(saveButton);
+        
+        
+
+        setContentPane(contentPane);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addButton || e.getSource() == subtractButton ||
+                e.getSource() == multiplyButton || e.getSource() == divideButton) {
+            // Get the numbers from the input fields
+            double num1 = Double.parseDouble(num1Field.getText());
+            double num2 = Double.parseDouble(num2Field.getText());
+
+            // Perform the appropriate operation based on the button clicked
+            double result = 0.0;
+            if (e.getSource() == addButton) {
+                result = num1 + num2;
+            } else if (e.getSource() == subtractButton) {
+                result = num1 - num2;
+            } else if (e.getSource() == multiplyButton) {
+                result = num1 * num2;
+            } else if (e.getSource() == divideButton) {
+                result = num1 / num2;
+            }
+
+            // Set the result in the result field
+            resultField.setText(Double.toString(result));
+        } else if (e.getSource() == loadButton) {
+            // Show a file chooser dialog to select the input XML file
+            JFileChooser fileChooser = new JFileChooser();
+            int result = fileChooser.showOpenDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                inputFile = fileChooser.getSelectedFile();
+                try {
+                    // Parse the input XML file and perform the calculations
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document doc = builder.parse(inputFile);
+                    NodeList problemList = doc.getElementsByTagName("problem");
+                    for (int i = 0; i < problemList.getLength(); i++) {
+                        Element problem = (Element) problemList.item(i);
+                        String operation = problem.getAttribute("operation");
+                        double num1 = Double.parseDouble(problem.getElementsByTagName("num1").item(0).getTextContent());
+                        double num2 = Double.parseDouble(problem.getElementsByTagName("num2").item(0).getTextContent());
+                        double result1 = 0.0;
+                        if (operation.equals("add")) {
+                            result1 = num1 + num2;
+                        } else if (operation.equals("subtract")) {
+                            result1 = num1 - num2;
+                        } else if (operation.equals("multiply")) {
+                            result1 = num1 * num2;
+                        } else if (operation.equals("divide")) {
+                            result1 = num1 / num2;
+                        }
+                        problem.getElementsByTagName("result").item(0).setTextContent(Double.toString(result1));
+                    }
+                    // Write the results to the output XML file
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+                    DOMSource source = new DOMSource(doc);
+                    outputFile = new File(inputFile.getParentFile(), "output.xml");
+                    StreamResult resultStream = new StreamResult(outputFile);
+                    transformer.transform(source, resultStream);
+                    JOptionPane.showMessageDialog(this, "Calculations completed and saved to output.xml");
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        } else if (e.getSource() == saveButton) {
+            // Show a file chooser dialog to select the output XML file
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setSelectedFile(new File("output.xml"));
+            int result = fileChooser.showSaveDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                outputFile = fileChooser.getSelectedFile();
+                try {
+                    // Parse the input XML file and perform the calculations
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document doc = builder.parse(inputFile);
+                    NodeList problemList = doc.getElementsByTagName("problem");
+                    for (int i = 0; i < problemList.getLength(); i++) {
+                        Element problem = (Element) problemList.item(i);
+                        String operation = problem.getAttribute("operation");
+                        double num1 = Double.parseDouble(problem.getElementsByTagName("num1").item(0).getTextContent());
+                        double num2 = Double.parseDouble(problem.getElementsByTagName("num2").item(0).getTextContent());
+                        double result1 = 0.0;
+                        if (operation.equals("add")) {
+                            result1 = num1 + num2;
+                        } else if (operation.equals("subtract")) {
+                            result1 = num1 - num2;
+                        } else if (operation.equals("multiply")) {
+                            result1 = num1 * num2;
+                        } else if (operation.equals("divide")) {
+                            result1 = num1 / num2;
+                        }
+                        problem.getElementsByTagName("result").item(0).setTextContent(Double.toString(result1));
+                    }
+                    // Write the results to the output XML file
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+                    DOMSource source = new DOMSource(doc);
+                    outputFile = new File(inputFile.getParentFile(), "output.xml");
+                    StreamResult resultStream = new StreamResult(outputFile);
+                    transformer.transform(source, resultStream);
+                    JOptionPane.showMessageDialog(this, "Calculations completed and saved to output.xml");
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CalculatorGUI calculator = new CalculatorGUI();
+        calculator.setVisible(true);
+    }
+}
---
+++ CalculatorValidator.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use XML::Simple;
+
+# Read the input XML file
+my $xml_file = shift @ARGV;
+my $xml = XMLin($xml_file);
+
+my $num_problems = 0;
+my $num_correct = 0;
+my $num_incorrect = 0;
+
+# Open the output text file for writing
+open(my $fh, '>', 'validationResults.txt') or die "Could not open validationResults.txt: $!";
+
+# Loop through each problem in the XML file and solve it
+foreach my $problem (@{$xml->{problem}}) {
+    my $num1 = $problem->{num1};
+    my $num2 = $problem->{num2};
+    my $operator = $problem->{operation};
+    my $expected_result = $problem->{result};
+
+    # Calculate the answer to the problem
+    my $actual_result;
+    if ($operator eq 'add') {
+        $actual_result = $num1 + $num2;
+#    } elsif ($operator eq 'subtract') {
+#        $actual_result = $num1 - $num2;
+#    } elsif ($operator eq 'multiply') {
+#        $actual_result = $num1 * $num2;
+#    } elsif ($operator eq 'divide') {
+#        $actual_result = $num1 / $num2;
+    }
+
+    # Check if the answer is correct
+    if ($expected_result == $actual_result) {
+        $num_correct++;
+        print "Correct!: The answer to $num1 $operator $num2 is listed as $expected_result, and the truth value was calculated as $actual_result\n";
+        print {$fh} "Correct!: The answer to $num1 $operator $num2 is listed as $expected_result, and the truth value was calculated as $actual_result\n";
+    } else {
+        $num_incorrect++;
+        print  "Error: The answer to $num1 $operator $num2 is listed as $expected_result, but the truth value was calculated as$actual_result\n";
+        print {$fh} "Error: The answer to $num1 $operator $num2 is listed as $expected_result, but the truth value was calculated as$actual_result\n";
+    }
+
+    $num_problems++;
+}
+
+
+
+# Report the overall results
+print "Total problems: $num_problems\n";
+print "Number correct: $num_correct\n";
+print "Number incorrect: $num_incorrect\n";
+print {$fh} "Total problems: $num_problems\n";
+print {$fh} "Number correct: $num_correct\n";
+print {$fh} "Number incorrect: $num_incorrect\n";
+
+
+# Close the output text file
+close($fh);
